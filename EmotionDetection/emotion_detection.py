73e7dd979af97f1c63a3b5400272882055a5627c
JaysreeSS <ssjaysree@gmail.com>
2026-06-25 12:53:27 +0530

    Implement emotion detection functionality and corresponding tests

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,29 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    obj = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(url, json = obj, headers = header)
+    
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        dominant_max = max(anger_score, disgust_score, fear_score, joy_score, sadness_score)
+        for i in formatted_response['emotionPredictions'][0]['emotion']:
+            if formatted_response['emotionPredictions'][0]['emotion'][i] == dominant_max:
+                dominant = i
+    
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant = None
+    
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant }
